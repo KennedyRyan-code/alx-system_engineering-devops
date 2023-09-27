6fd4fb74ef14be6b4ad7b33b230554ff8cffae26
KennedyRyan-code <kenryan49@gmail.com>
2023-09-27 18:10:58 +0300

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 # Kill a running process killmenow from the process table
 exec { 'killmenow':
-  command => 'pkill killmenow',
-  onlyif  => 'pgrep killmenow',
+  command  => 'pkill killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
 }
