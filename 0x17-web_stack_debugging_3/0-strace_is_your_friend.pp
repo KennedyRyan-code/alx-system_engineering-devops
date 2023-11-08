68b8c4f77ab5db8ce14f83fdac3e24ad6682107d
KennedyRyan-code <kenryan49@gmail.com>
2023-11-08 21:10:20 +0300

    Strace is ur friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Puppet manifest to fix Apache 500 error issue
 
 exec { 'fix-apache-issue':
-  command     => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path        => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  refreshonly => true,
+  path    => '/usr/local/bin/:/bin/'
 }
